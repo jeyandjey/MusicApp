7287adaf4a46a463c8ab2a5a2a86621c40023de2
jeyandjey <jennifersampaio225@gmail.com>
2025-06-05 21:58:55 -0300

    primeiro commit

--- app/src/main/java/br/com/etecia/myapp/pag_rock.java
+++ app/src/main/java/br/com/etecia/myapp/pag_rock.java
@@ -14,7 +14,7 @@ public class pag_rock extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_pag_rock);
+        setContentView(R.layout.pag_rock_layout);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
